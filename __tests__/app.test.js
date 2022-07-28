a2b2b29abb6dcd17ad1d398a8ff5b5f8dc84a36d
Sean Moriarty <spmoriarty1@gmail.com>
2022-07-28 15:11:51 -0700

    built outline of test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,42 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('games routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/games should return a list of games', async () => {
+    const res = await request(app).get('/games');
+    const expected = [{
+      id: ,
+      name: ,
+      type: ,
+      players: ,
+    },
+  {
+      id: ,
+      name: ,
+      type: ,
+      players: ,
+  }
+];
+console.log(res.body);
+    expect(res.body[1]).toEqual({
+      id: '2',
+      name: ,
+      type: ,
+      players: ,
+    });
+  });
+  it('/games/:id should return game detail', async () =>{
+    const res = await request(app).get('/games/1');
+    const XGAME = {
+      id: ,
+      name: ,
+      type: ,
+      players: ,
+    };
+    expect(res.body).toEqual(XGAME);
   });
   afterAll(() => {
     pool.end();
